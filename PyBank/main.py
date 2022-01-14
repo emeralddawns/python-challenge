94fa4c4d63ad1751b2010ef3ecb45438f37c04b8
emeralddawns <93622526+emeralddawns@users.noreply.github.com>
2022-01-14 15:43:32 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,18 +15,29 @@ import csv
 
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-print("here are some words")
+net = 0
+months = 0
+month_change = []
+
+# prove it is being read
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    print(csvreader)
+    ## print(csvreader)
 
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader) #next consumes a row
-    print(f"CSV Header: {csv_header}")
+    ## print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
-    for row in csvreader:
-        print(row)
+    for row in csvreader:        
+        ## print(row)
+        months += 1
+        net += float(row[1])
+        net_dollar = "${:.2f}".format(net) #"${:,.2f}".format(net) format with commas
+        month_change[row-1] = float()
+
+    print (f"{months}")
+    print(f"{net_dollar}")
