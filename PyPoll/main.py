3859f6fc59b2457c3968920639e28bca24b74299
emeralddawns <93622526+emeralddawns@users.noreply.github.com>
2022-01-14 23:06:24 -0600

    Everything but the winner
    Also I don't like how the summary is printed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -18,6 +18,11 @@ output_path = os.path.join("Analysis", "Election_Results.txt")
 
 #initialize values
 votes = 0
+candidates = []
+vote_count = []
+# percentage = []
+i=int(0)
+j=int(0)
 
 with open(csvpath) as csvfile:
 
@@ -29,22 +34,52 @@ with open(csvpath) as csvfile:
 
     # Read each row of data after the header, make calculations, and store values
     for row in csvreader:
-        votes += 1
+        votes += 1                          #count the total number of votes
 
+        if row[2] not in candidates:        
+            candidates.append(row[2])       #create list of unique candidates
+            vote_count.append(int(1))       #create list of vote counts for unique candidates
+        else:
+            i = 0    
+            for name in candidates:
+                if name == row[2]:
+                    vote_count[i] += 1      # +1 to a vote count for a unique candidate
+                i += 1
 
+    percent_vote = [(x / votes) for x in vote_count]      #calculate the percentage of votes received
+
+    print(f"{candidates}")
+    print(f"{vote_count}")
+    print(f"{percent_vote}")
 #summarize the data in one place so that the terminal and output .txt receive the same information
 line = (f"-------------------------")
-summary = [(f"Election Results"),
+summary_header = [(f"Election Results"),
             (line),
             (f"Total Votes: {votes}"),
             (line),]
 
+#for names in candidates:
+#    percent_vote[j] = "{:.3%}".format(percent_vote[j])
+#    print(f"{candidates[j]}: {percent_vote[j]} ({vote_count[j]})")
+#    j+=1
+
 #print to the terminal
-for line in summary:
+for line in summary_header:
     print(line)
+
+for names in candidates:
+    percent_vote[j] = "{:.3%}".format(percent_vote[j])
+    print(f"{candidates[j]}: {percent_vote[j]} ({vote_count[j]})")
+    j+=1
 
 #print to the .txt file
 with open(output_path, "w", newline = '') as datafile:
     writer = csv.writer(datafile)
-    for line in summary:
+    for line in summary_header:
         writer.writerow([line])
+
+    j = int(0)
+    for names in candidates:
+#        percent_vote[j] = "{:.0%}".format(percent_vote[j])
+        writer.writerow([f"{candidates[j]}: {percent_vote[j]} ({vote_count[j]})"])
+        j += 1
